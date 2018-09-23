0cc01ff534e268be4fd09156f7df4b6c9a9767e5
alvinlukmanNH <galinx20gmail.com>
2018-09-23 19:42:28 +0700

    Pecahan

---
+++ src/if3/pkg10117095/latihan8/pecahan/IF310117095Latihan8Pecahan.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if3.pkg10117095.latihan8.pecahan;
+
+/**
+ *
+ * @author 
+ * NAMA         : Alvin Lukman Nulhakim
+ * KELAS        : IF-3
+ * NIM          : 10117095
+ * Deklarasi    : Program ini berisi cara penulisan type data Pecahan
+ *                pada program
+ */
+public class IF310117095Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float : " + f);
+        System.out.println("Double : " + d);
+        
+    }
+    
+}
